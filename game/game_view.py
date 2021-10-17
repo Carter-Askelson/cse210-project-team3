9fc79c03d6b602874b7182bd9465a623546a7fab
Emma Burkett <bur20089@byui.edu>
2021-10-16 23:10:04 -0600

    sound

--- game/game_view.py
+++ game/game_view.py
@@ -1,20 +1,25 @@
 import arcade
 
-
 class GameView(arcade.View):
     def __init__(self):
         super().__init__()
         self.cards_list = None
+        self.sound = arcade.Sound(":resources:sounds/laser1.wav")
+        self.pan = 0
+        self.volume = 1
 
     def setup(self):
         self.cards_list = arcade.SpriteList()
+    
+    def play(self):
+        """ Play """
+        self.sound.play(pan=self.pan, volume=self.volume)
 
     def on_draw(self):
         arcade.start_render()
         arcade.set_background_color(arcade.color.AMAZON)
         arcade.draw_text("Blackjack game will be here.", 65, 425, arcade.color.BLACK, 16)
-        self.cards_list = arcade.SpriteList()
-        self.cards_list.draw()
+        ##self.cards_list.draw()
 
     
     def draw_card(self):
