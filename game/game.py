dd4c733d8e6c1b162efa14ac6fac92c11b59746a
prograhamming <grahamhwalker@outlook.com>
2021-10-16 23:12:52 -0500

    redoing game windows to views

--- game/game.py
+++ game/game.py
@@ -1,4 +1,5 @@
 import arcade
+from .game_rules_view import GameRules
 
 
 class Game(arcade.Window):
@@ -15,9 +16,10 @@ class Game(arcade.Window):
     def on_draw(self):
         arcade.start_render()
         arcade.set_background_color(arcade.color.AMAZON)
-        arcade.draw_text("Welcome to Blackjack", 50, 425, arcade.color.BLACK, 34)
+        arcade.draw_text("Welcome to Blackjack", 65, 425, arcade.color.BLACK, 34)
         arcade.draw_text("Press 'E' to enter into a game of Blackjack", 100, 325, arcade.color.BLACK, 16)
         arcade.draw_text("Press 'L' to learn the rules of Blackjack", 100, 225, arcade.color.BLACK, 16)
+
         arcade.finish_render()
 
     def on_key_press(self, symbol: int, modifiers: int):
@@ -34,4 +36,4 @@ class Game(arcade.Window):
         elif symbol == arcade.key.L:
             # Go to game rules window
             print("You pressed L")
-            pass
+            GameRules.on_draw(self)
