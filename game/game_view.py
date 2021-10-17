54cc142af6346d9df136886629eb2d0017804feb
Emma Burkett <bur20089@byui.edu>
2021-10-16 23:27:32 -0600

    card!

--- game/game_view.py
+++ game/game_view.py
@@ -10,10 +10,12 @@ class GameView(arcade.View):
         arcade.start_render()
         arcade.set_background_color(arcade.color.AMAZON)
         arcade.draw_text("Blackjack game will be here.", 65, 425, arcade.color.BLACK, 16)
+        self.draw_card()
         self.cards_list.draw()
     
     def draw_card(self):
         card_seven = arcade.Sprite(":resources:images/cards/cardHearts7.png", 1)
-        card_seven.center_x = 50
-        card_seven.center_y = 50
+        card_seven.center_x = 100
+        card_seven.center_y = 100
         self.cards_list.append(card_seven)
+    
