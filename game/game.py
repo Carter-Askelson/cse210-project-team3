8cfd4099927e5ef45fa910661b38aab32bbd4748
prograhamming <grahamhwalker@outlook.com>
2021-10-16 22:36:42 -0500

    adding menu text

--- game/game.py
+++ game/game.py
@@ -15,5 +15,7 @@ class Game(arcade.Window):
     def on_draw(self):
         arcade.start_render()
         arcade.set_background_color(arcade.color.AMAZON)
-        arcade.draw_text("Welcome to Blackjack", 43, 425, arcade.color.BLACK, 34)
+        arcade.draw_text("Welcome to Blackjack", 50, 425, arcade.color.BLACK, 34)
+        arcade.draw_text("Press 'E' to enter into a game of Blackjack", 100, 325, arcade.color.BLACK, 16)
+        arcade.draw_text("Press 'L' to learn the rules of Blackjack", 100, 225, arcade.color.BLACK, 16)
         arcade.finish_render()
