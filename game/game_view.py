a93291d338baa13d54c65467b31b3a6f1bb89519
Emma Burkett <77114845+EmmaBurkett@users.noreply.github.com>
2021-10-16 23:29:56 -0600

    add card

--- game/game_view.py
+++ game/game_view.py
@@ -1,12 +1,20 @@
 import arcade
 
-
 class GameView(arcade.View):
     def __init__(self):
         super().__init__()
-
-
+        self.cards_list = None
+        self.cards_list = arcade.SpriteList()
+    
     def on_draw(self):
         arcade.start_render()
         arcade.set_background_color(arcade.color.AMAZON)
         arcade.draw_text("Blackjack game will be here.", 65, 425, arcade.color.BLACK, 16)
+        self.draw_card()
+        self.cards_list.draw()
+    
+    def draw_card(self):
+        card_seven = arcade.Sprite(":resources:images/cards/cardHearts7.png", 1)
+        card_seven.center_x = 100
+        card_seven.center_y = 100
+        self.cards_list.append(card_seven)
