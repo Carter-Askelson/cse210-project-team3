485fa5ce6531761584159085788045fd7e451def
Carter-Askelson <Askelsonc@gmail.com>
2021-10-20 12:19:16 -0600

    added hiding the dealer's first card

--- game/card.py
+++ game/card.py
@@ -1,6 +1,7 @@
 import arcade
 
 
+
 class Card(arcade.Sprite):
     """ Card sprite """
 
@@ -10,26 +11,24 @@ class Card(arcade.Sprite):
         # Attributes for suit and value
         self.suit = suit
         self.value = value
-        self.face_down_image = "cardBack_red4.png"
+        self.face_down_image = ":resources:images/cards/cardBack_red4.png"
 
         # Image to use for the sprite when face up
         self.image_file_name = f":resources:images/cards/card{self.suit}{self.value}.png"
         #need to make first dealer card face down
-        """ self.is_face_up = False
-        super().__init__(FACE_DOWN_IMAGE, scale, hit_box_algorithm="None")
+        self.is_face_up = True
+        super().__init__(self.image_file_name, scale, hit_box_algorithm="None")
 
     def face_down(self):
-        #Turn card face-down
-        self.texture = arcade.load_texture(FACE_DOWN_IMAGE)
+        """ Turn card face-down """
+        self.texture = arcade.load_texture(self.face_down_image)
         self.is_face_up = False
 
     def face_up(self):
-        #Turn card face-up
+        """ Turn card face-up """
         self.texture = arcade.load_texture(self.image_file_name)
-        self.is_face_up = True """
+        self.is_face_up = True
 
-        # Call the parent
-        super().__init__(self.image_file_name, scale, hit_box_algorithm="None")
 
     def __repr__(self):
             x = self.value + " of " \
