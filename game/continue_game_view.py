f47adaefdd7ecae64eaef08ce5ea9be1449585aa
Emma Burkett <bur20089@byui.edu>
2021-10-26 21:31:13 -0600

    Alex's work + some NumPy strings

--- game/continue_game_view.py
+++ game/continue_game_view.py
@@ -1,4 +1,5 @@
 import arcade
+import time
 
 
 class ContinueGameView(arcade.View):
@@ -65,18 +66,21 @@ class ContinueGameView(arcade.View):
             self (ContinueGameView): an instance of ContinueGameView.
         """
         arcade.start_render()
-
+        
         if self.victory:
+            time.sleep(2)
             arcade.draw_text("You Won", self.window_width / 2, self.window_width / 2, arcade.color.BLUE, 64,
                              anchor_x="center")
             arcade.draw_text("Would you like to play again? [Y/N]", self.window_width / 2, self.window_width / 2 - 75,
                              arcade.color.BLUE, 24, anchor_x="center")
         elif self.defeat:
+            time.sleep(2)
             arcade.draw_text("You Lost", self.window_width / 2, self.window_width / 2, arcade.color.RED, 64,
                              anchor_x="center")
             arcade.draw_text("Would you like to play again? [Y/N]", self.window_width / 2, self.window_width / 2 - 75,
                              arcade.color.RED, 24, anchor_x="center")
         if self.game_over:
+            time.sleep(2)
             arcade.draw_text("All out of Chips", self.window_width / 2, self.window_width / 2, arcade.color.BLACK, 64,
                              anchor_x="center")
             arcade.draw_text("Press [Q] to quit game, (in shame)", self.window_width / 2, self.window_width / 2 - 75,
